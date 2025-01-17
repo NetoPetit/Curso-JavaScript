6eca7b73e6bd56541995b25bdbfde37d7a167484
NetoPetit <cidao_petit5@hotmail.com>
2025-01-17 19:13:48 -0300

    atualização do curso JS

---
+++ Aula19/index.js
@@ -0,0 +1,7 @@
+/*
+Tipos de dados:
+
+Primitivos(imutaveis)- string,number, boolean, undefined,
+null (bigint, symbol) - Valores copiados
+
+Referência(mutável)- array, object, function - Valores passados por referência*/
