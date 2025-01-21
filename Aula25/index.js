60537036bc13c9760551572c0135162465d8bd5f
NetoPetit <cidao_petit5@hotmail.com>
2025-01-21 18:59:41 -0300

    atualização do curso JS

---
+++ Aula25/index.js
@@ -0,0 +1,11 @@
+const numero = 17;
+
+if (numero >= 0 && numero <= 10) {
+    console.log('O numero esta entre 0 e 10');
+} else if (numero >= 11 && numero <= 20) {
+    console.log('O numero esta entre 11 e 20');
+} else if (numero >= 21 && numero <= 30) {
+    console.log('O numero esta entre 21 e 30');
+} else {
+    console.log('Esta fora do range de 0 e 30');
+}
